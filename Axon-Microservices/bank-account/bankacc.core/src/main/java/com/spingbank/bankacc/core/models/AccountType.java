b65dbeaf5d373ed5ac0c8f6b2651ef149da92d07
hao <huynhtamhao@gmail.com>
2021-05-24 15:37:03 +0700

    feat: add bank account models

---
+++ Axon-Microservices/bank-account/bankacc.core/src/main/java/com/spingbank/bankacc/core/models/AccountType.java
@@ -0,0 +1,5 @@
+package com.spingbank.bankacc.core.models;
+
+public enum AccountType {
+    SAVINGS, CURRENT
+}
---
+++ Axon-Microservices/bank-account/bankacc.core/src/main/java/com/spingbank/bankacc/core/models/BankAccount.java
@@ -0,0 +1,24 @@
+package com.spingbank.bankacc.core.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class BankAccount {
+    @Id
+    private String id;
+    private String accountHolderId;
+    private LocalDate creationDate;
+    private AccountType accountType;
+    private double balance;
+}
